3056c607ff62eb96a5f54dfb19070b07484a7053
adrienlementheour <adrien.le.mentheour@gmail.com>
2018-12-02 22:33:48 +0100

    🎨 Projects / Home

--- antoine-denis/pages/index.vue
+++ antoine-denis/pages/index.vue
@@ -1,62 +1,46 @@
 <template>
   <section class="container">
     <div>
-      <logo/>
       <h1 class="title">
         antoine-denis
       </h1>
       <h2 class="subtitle">
         Portfolio d&#39;Antoine Denis, graphiste
       </h2>
-      <ul id="example-1">
-        <li v-for="project in projects" :key="project.data.id">
+      <div class="projects">
+        <div v-for="project in projects" :key="project.data.id" class="project">
+          {{ project.data }}
+          <div v-if="project.data.slider.length == 1">
+            <img :src="project.data.slider[0].slider_image.url" :alt="project.data.slider[0].slider_image.alt">
+          </div>
+          <div v-else>
+            Slider
+          </div>
           {{ project.data.nom_du_projet[0].text }}
-        </li>
-      </ul>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
+        </div>
       </div>
     </div>
   </section>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue';
 import Prismic from 'prismic-javascript';
 
 export default {
-  components: {
-    Logo
-  },
-  // created() {
-  //   var apiEndpoint = 'https://antoine-denis.cdn.prismic.io/api/v2';
-  //   Prismic.getApi(apiEndpoint)
-  //     .then(function(api) {
-  //       return api.query(''); // An empty query will return all the documents
-  //     })
-  //     .then(
-  //       function(response) {
-  //         console.log('Documents: ', response.results);
-  //       },
-  //       function(err) {
-  //         console.log('Something went wrong: ', err);
-  //       }
-  //     );
+  // components: {
   // },
   async asyncData() {
     var apiEndpoint = 'https://antoine-denis.cdn.prismic.io/api/v2';
+    let home = {};
     let projects = {};
 
     const api = await Prismic.getApi(apiEndpoint);
-    // const result = await api.getByUID('document.type', 'projet');
-    // const result = await api.getByID('XAPjzRAAACcAYMxN');
+    await api
+      .query(Prismic.Predicates.at('document.type', 'home'))
+      .then(function(response) {
+        // response is the response object, response.results holds the documents
+        home = response.results;
+      });
     await api
       .query(Prismic.Predicates.at('document.type', 'projet'), {
         orderings: '[my.projet.date desc]'
@@ -64,13 +48,9 @@ export default {
       .then(function(response) {
         // response is the response object, response.results holds the documents
         projects = response.results;
-        console.log(projects);
       });
 
-    // preview.description = result.data.description[0].text;
-    // preview.title = result.data.title[0].text;
-
-    return { projects };
+    return { home, projects };
   }
 };
 </script>
@@ -100,9 +80,5 @@ export default {
   color: #526488;
   word-spacing: 5px;
   padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
 }
 </style>
