a43f55c44f139a33929ef0dd361c90f6657a21f3
adrienlementheour <adrien.le.mentheour@gmail.com>
2019-02-24 22:59:52 +0100

    💄 gradient image

--- components/Header.vue
+++ components/Header.vue
@@ -51,7 +51,8 @@ export default {
         left: 0;
         width: 100%;
         height: 190px;
-        background: $gradient;
+        background: url('~static/bg-gradient.png') repeat-x 0 0;
+        background-size: auto 100%;
         z-index: -1;
         pointer-events: none;
     }
