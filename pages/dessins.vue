f50418ce08b6dcf3c96cbfcfe9b5165aec2b7b87
menthe <adrien.le.mentheour@gmail.com>
2023-08-29 10:07:22 +0200

    💄 btn close

--- pages/dessins.vue
+++ pages/dessins.vue
@@ -172,9 +172,9 @@ export default {
 .btn-close {
     position: absolute;
     top: 0;
-    right: 0;
-    width: 50px;
-    height: 50px;
+    right: 8px;
+    width: 40px;
+    height: 40px;
     z-index: 2;
 }
 
